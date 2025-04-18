d9d33aeea1438da211448007d3e0e2ccb891dac6
DuskSwan <aquark@buaa.edu.cn>
2025-04-18 13:31:45 +0800

    初始化，并创建调用Gemini的示例

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # MCP-Explorer
+
+本项目意在学习创建一个MCP客户端，要实现的内容包括
+
+- 通过API调用远端大模型
+- 本地部署大模型
+- 建立MCP客户端
+- 创建一些本地MCP服务器供使用
+
+## 相关库
+
+dotenv: 加载.env文件以获取API（环境变量）
+
+google-genai: 使用 Python 3.9 及更高版本时，请使用以下 pip 命令安装 google-genai 软件包 
+pip install -q -U google-genai
+
+## 参考
+
+[Gemini API 使用入门](https://ai.google.dev/gemini-api/docs/text-generation?hl=zh-cn)
---
+++ examples/Gemini.py
@@ -0,0 +1,48 @@
+from google import genai
+import os
+import dotenv
+dotenv.load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+def get_response(prompt, model_name="gemini-2.0-flash"):
+    client = genai.Client(api_key=GEMINI_API_KEY)
+
+    response = client.models.generate_content(
+        model=model_name,
+        contents=prompt,
+    )
+
+    return response.text
+
+def get_response_with_stream(prompt, model_name="gemini-2.0-flash"):
+    client = genai.Client(api_key=GEMINI_API_KEY)
+
+    response = client.models.generate_content_stream(
+        model=model_name,
+        contents=[prompt]
+    )
+    for chunk in response:
+        print(chunk.text, end="")
+
+def generate_chat():
+    client = genai.Client(api_key=GEMINI_API_KEY)
+    chat = client.chats.create(model="gemini-2.0-flash")
+
+    response = chat.send_message("I have 2 dogs in my house.")
+    print(response.text)
+
+    response = chat.send_message("How many paws are in my house?")
+    print(response.text)
+
+    for message in chat.get_history():
+        print(f'role - {message.role}',end=": ")
+        print(message.parts[0].text)
+
+if __name__ == "__main__":
+    
+    # response = get_response("What is the capital of France?")
+    # print(f"Response: {response}")
+
+    # get_response_with_stream("Explain the theory of relativity in simple terms.")
+
+    generate_chat()
